c3de2e0fa1811505ba970a6408d81b8af20ba79e
Sandross <sandrosalasfilho3@gmail.com>
2024-09-01 14:55:53 -0300

    feat: adding layout component

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import styles from './style.module.scss';
+export default function Layout() {
+  return (
+    <div className={styles.layoutContainer}>
+      
+    </div>
+  );
+}
