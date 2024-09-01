0b4de2dd85edab74cbc2fba6fe6652c989f7f549
Sandross <sandrosalasfilho3@gmail.com>
2024-09-01 13:56:34 -0300

    feat: initial redux config, with AsyncThunks

---
+++ src/store/reducers/assets.ts
@@ -0,0 +1,46 @@
+import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+
+interface Asset {
+  id: string;
+  name: string;
+}
+
+interface AssetsState {
+  assets: Asset[];
+}
+
+const initialState: AssetsState = {
+  assets: [],
+};
+
+export const getAllAssets = createAsyncThunk<Asset[], void>(
+  'assets/getAllAssetsThunk',
+  async () => {
+    return [];
+  }
+);
+
+export const getAssetsByRegion = createAsyncThunk<Asset[], string>(
+  'assets/getAssetsByRegion',
+  async (region: string) => {
+    return region || [];
+  }
+);
+
+const assetsReducer = createSlice({
+  name: 'assetsReducer',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(getAllAssets.fulfilled, (state, action: PayloadAction<Asset[]>) => {
+      state.assets = action.payload;
+    });
+    builder.addCase(getAssetsByRegion.fulfilled, (state, action: PayloadAction<Asset[]>) => {
+      state.assets = action.payload;
+    });
+  },
+});
+
+export type RootState = ReturnType<typeof assetsReducer.reducer>;
+
+export default assetsReducer.reducer;
